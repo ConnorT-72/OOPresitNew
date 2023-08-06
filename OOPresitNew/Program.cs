5854f4104faa19220ad8545563a568820310c09d
gamer <gamer@DESKTOP-0OB23D4>
2023-08-06 02:25:24 +0100

    Add project files.

---
+++ OOPresitNew/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class TestScore
+{
+    public static void Main()
+    {
+        string name;
+        double score;
+
+        Console.WriteLine("Welcome to the Acme Student Test Score commentator");
+
+        Console.WriteLine("Enter your name: ");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Enter the test score: ");
+        if (double.TryParse(Console.ReadLine(), out score))
+        {
+            if (score >= 0 && score <= 100)
+            {
+                Console.WriteLine("Hello {0}", name);
+                Console.WriteLine("You scored {0}", score);
+
+                if (score < 40)
+                {
+                    Console.WriteLine("This is a FAIL score");
+                }
+                else
+                {
+                    Console.WriteLine("This is a PASS score");
+                    if (score >= 75)
+                    {
+                        Console.WriteLine("You did very well!");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Oh dear - you have entered an invalid test score");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid numeric test score.");
+        }
+
+        // Repeat the code to allow the user to enter new input
+        Main();
+    }
+}
